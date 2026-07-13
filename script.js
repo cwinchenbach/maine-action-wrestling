3f51d991428c11b96ac2616e8fc4f2597ac11c59
cwinchenbach <winchenbachcole@gmail.com>
2026-07-12 23:16:46 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-btn')?.addEventListener('click', () => {
-  document.querySelector('nav').classList.toggle('open');
-});
-
-document.querySelector('.subscribe-btn')?.addEventListener('click', () => {
-  const button = document.querySelector('.subscribe-btn');
-  button.textContent = 'You’re ringside ✓';
-  button.style.background = '#2538c7';
-});
